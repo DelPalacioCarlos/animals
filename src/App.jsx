23bf91ac79c5c802aa41a6259476c9ee518a5f55
DelPalacioCarlos <carlosdelpalacio5@gmail.com>
2025-04-01 18:37:15 -0700

    Agrege codigo App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react"
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import AnimalsShow from "./animalsShow"
 
+function getRandomAnimal(){
+  const animals = ['cat', 'bird', 'dog', 'gator', 'horse']
+
+  return animals[Math.floor(Math.random() * animals.length)]
+}
+
+console.log(getRandomAnimal())
+
+function App() {
+
+  const [animals, setAnimals] = useState ([])
+
+  const handleClick =  () => {
+    setAnimals([...animals, getRandomAnimal()])
+  }
+
+  const renderAnimals =  animals.map((animal, index) => {
+    return <AnimalsShow type={animal} key={index}/> 
+
+  })
   return (
-    <>
-      
-    </>
+    <div className="app">
+      <button onClick={handleClick}>Add Animal</button>
+      <div className="animals-list">
+        {renderAnimals}
+      </div>
+    </div>
   )
 }
+
+
+
 
 export default App
